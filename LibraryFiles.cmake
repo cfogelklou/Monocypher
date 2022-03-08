305e7842c783936d78f6129e0d7bfb0232237fd1
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-03-08 06:00:17 +0100

    First version with cmake support.

--- LibraryFiles.cmake
+++ LibraryFiles.cmake
@@ -1,34 +1,11 @@
 
 set(MC_COMMON_SRC ${MC_TOP_DIR}/src)
 
-#defines we need to build libsodium
-if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  #For MSVC less than or equal to 10.0, "inline" doesn't exist.
-  if (MSVC_VERSION)
-    if (MSVC_VERSION LESS 1601)
-      add_definitions(-Dinline=__inline)
-    endif ()
-  endif ()
-  add_definitions("/wd4146 /wd4244 /wd4996 -D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING")
-endif ()
-
-if (WIN32)
-    #set(FREERTOS_PORT 1)
-    add_definitions(-DWIN32)
-elseif (APPLE)
-    add_definitions(-DAPPLE=1 -D__APPLE__=1 -DTARGET_OS_OSX=1)
-elseif (UNIX)
-    add_definitions(-DNATIVE_LITTLE_ENDIAN)
-elseif (EMSCRIPTEN)
-    add_definitions(-DNATIVE_LITTLE_ENDIAN)    
-endif ()
-
-if (EMSCRIPTEN)
-  add_definitions(-DEMSCRIPTEN -D__EMSCRIPTEN__)
-endif(EMSCRIPTEN)
-
-
-
+add_definitions("-DUSE_MONOCYPHER
+-DED25519_CUSTOMHASH
+-DED25519_TEST
+-DED25519_NO_INLINE_ASM
+-DED25519_FORCE_32BIT")
 
 # USE recursive search to add all libsodium files.
 file(GLOB_RECURSE MONOCYPHER_REC_SRC
@@ -37,7 +14,7 @@ file(GLOB_RECURSE MONOCYPHER_REC_SRC
 
 set(MONOCYPHER_SRC
     ${MONOCYPHER_REC_SRC}
-
+    ${MC_TOP_DIR}/LibraryFiles.cmake
 )
 
 list(REMOVE_DUPLICATES MONOCYPHER_SRC)
